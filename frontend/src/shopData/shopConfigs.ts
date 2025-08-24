4fbac44b29b8bc02824e134dc789272f3f2b214b
chen126888 <41716407+chen126888@users.noreply.github.com>
2025-08-25 04:53:40 +0800

    更新ex商店說明

--- frontend/src/shopData/shopConfigs.ts
+++ frontend/src/shopData/shopConfigs.ts
@@ -102,7 +102,7 @@ export const shopConfigs: Record<ShopType, ShopConfig> = {
 僅列出彩裝，彩裝順序參考自煌靈大大
 至於一般EX裝備請按自身需求購買
 
-📹 詳細說明請查詢以下影片：
+詳細說明請查詢以下影片(作者YT煌靈)：
 彩裝：[影片連結](https://youtu.be/gxmhkdtAmrU?si=ygUTDkyo9u2JJAw3)
 一般：[影片連結](https://youtu.be/s0K2FLYTxIE?si=lq0UBZ2n6V3bvmgs)`,
     currency: 'jewel',
